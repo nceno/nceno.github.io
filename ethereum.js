8ae22192427a4744322ca8359e47aa39c5bd92e6
nance2uiuc <nance2uiuc@gmail.com>
2018-10-28 22:06:30 +0800

    asdf

--- ethereum.js
+++ ethereum.js
@@ -320,7 +320,7 @@
                       //prompt for signing deposit
                       //get address of most recent goal created by fitbitID
                       GoalFactory.getLastGoalByFitbitID(
-                        $("#fitbitID").val(),
+                        "1"+$("#fitbitID").val(),
                         function(error, result) {
                           if (!error){
                             //pass it and the intended stake to deposit
