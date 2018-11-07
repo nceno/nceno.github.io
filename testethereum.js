880f895577a9752d1b18bab4a9c32dfa2e2dde0a
nance2uiuc <nance2uiuc@gmail.com>
2018-11-07 15:02:34 +0800

    asdf

--- testethereum.js
+++ testethereum.js
@@ -5,7 +5,7 @@
         //web3 = new Web3(new Web3.providers.HttpProvider("https://rinkeby.infura.io/v3/9db967faa260482782c435096a818865")); //rinkeby 
         //web3 = new Web3(new Web3.providers.HttpProvider("https://mainnet.infura.io/v3/9db967faa260482782c435096a818865")); //mainnet
          
-        web3.eth.defaultAccount = web3.eth.accounts[2];
+        web3.eth.defaultAccount = web3.eth.accounts[0];
 
         var GoalFactoryContract = web3.eth.contract([
   {
