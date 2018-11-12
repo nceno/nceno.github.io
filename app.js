3d14840bc4b1567414ab1921be9e97006846d04e
nance2uiuc <nance2uiuc@gmail.com>
2018-11-12 21:13:04 +0800

    asdf

--- app.js
+++ app.js
@@ -1,16 +1,12 @@
 // get the url
-var url = window.location.href;
+//var url = window.location.hash;
 //console.log(url);
 
 //getting the access token from url
-var access_token2 = url.split('#')[1];
-var access_token1 = access_token2.split('=')[1];
-var access_token = access_token1.split('&')[0];
+var access_token = window.location.href.split('#')[1].split('=')[1].split('&')[0];
 
 // get the userid
-var userId2 = url.split('#')[1];
-var userId1= userId2.split('=')[2];
-var userId = userId1.split('&')[0];
+var userId = window.location.href.split('#')[1].split('=')[2].split('&')[0];
 
 console.log(access_token);
 console.log(userId);
