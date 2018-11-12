6bfc6c3f6cf060847ff543a2dfe7299a9e5547e0
nance2uiuc <nance2uiuc@gmail.com>
2018-11-12 20:12:00 +0800

    asdf

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
-// get the url
-var url = window.location.hash;
+var access_token= eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMkQ1U04iLCJzdWIiOiI2TTM1NkIiLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJybG9jIHJhY3QgcmhyIHJwcm8iLCJleHAiOjE1NzM1NTgwNTMsImlhdCI6MTU0MjAyNDQ2NH0.Kke008tn1ekWY57ekez0298feEjhNSKlKyXMjDQSWX4;
+var userId= 6M356B;
 
-//getting the access token from url
-var access_token = url.split("#")[1].split("=")[1].split("&")[0];
 
-// get the userid
-var userId = url.split("#")[1].split("=")[2].split("&")[0];
-
-console.log(access_token);
-console.log(userId);
+var xhr = new XMLHttpRequest();
+xhr.open('GET', 'httpss://api.fitbit.com/1/user/'+ userId +'/activities/heart/date/today/1w.json');
+xhr.setRequestHeader("Authorization", 'Bearer ' + access_token);
+xhr.onload = function() {
+   if (xhr.status === 200) {
+      console.log(xhr.responseText);
+      document.write(xhr.responseText);
+   }
+};
+xhr.send()
