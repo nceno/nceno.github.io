df2b1d06d56eb2834b2c09e83b82d12e8cac3fb3
nance2uiuc <nance2uiuc@gmail.com>
2018-11-12 14:17:30 +0800

    sadf

---
+++ settle.js
@@ -0,0 +1,9 @@
+//call fitbit api with user creds
+
+//store response as var
+
+//strip response to activeMinutes components
+
+//send to smart contract "settle(_submission)"
+
+//settle(_submission) compares _submission to goal minutes, triggers payout accordingly
