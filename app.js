3c80f86eaeb3702b9f4de1a9e1a789d4e0fdf14b
nance2uiuc <nance2uiuc@gmail.com>
2018-11-13 18:36:46 +0800

    adsg

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+///////////////call fitbit api with user creds
 //getting the access token from url
 var access_token = window.location.href.split('#')[1].split('=')[1].split('&')[0];
 
@@ -13,8 +14,13 @@ xhr.open('GET', 'https://api.fitbit.com/1/user/'+ userId +'/activities/heart/dat
 xhr.setRequestHeader("Authorization", 'Bearer ' + access_token);
 xhr.onload = function() {
    if (xhr.status === 200) {
-      console.log(xhr.responseText);
-      document.write(xhr.responseText);
+      //console.log(xhr.responseText);
+      //document.write(xhr.responseText);
+      var obj = responseText;
+      console.log(userId +"'s active minutes for "+ obj[0]["activities-heart"][0].dateTime);
+	  console.log(obj[0]["activities-heart"][0].value.heartRateZones[1]);
+	  console.log(obj[0]["activities-heart"][0].value.heartRateZones[2]);
+	  console.log(obj[0]["activities-heart"][0].value.heartRateZones[3]);
    }
 };
 xhr.send()
--- settle.js
+++ settle.js
@@ -1,8 +1,32 @@
-//call fitbit api with user creds
+///////////////call fitbit api with user creds
+//getting the access token from url
+var access_token = window.location.href.split('#')[1].split('=')[1].split('&')[0];
 
-//store response as var
+// get the userid
+var userId = window.location.href.split('#')[1].split('=')[2].split('&')[0];
 
-//strip response to activeMinutes components
+console.log(access_token);
+console.log(userId);
+
+
+var xhr = new XMLHttpRequest();
+xhr.open('GET', 'https://api.fitbit.com/1/user/'+ userId +'/activities/heart/date/today/1d.json');
+xhr.setRequestHeader("Authorization", 'Bearer ' + access_token);
+xhr.onload = function() {
+   if (xhr.status === 200) {
+      //console.log(xhr.responseText);
+      //document.write(xhr.responseText);
+      var obj = responseText;
+      console.log(userId +"'s active minutes for "+ obj[0]["activities-heart"][0].dateTime);
+	  console.log(obj[0]["activities-heart"][0].value.heartRateZones[1]);
+	  console.log(obj[0]["activities-heart"][0].value.heartRateZones[2]);
+	  console.log(obj[0]["activities-heart"][0].value.heartRateZones[3]);
+   }
+};
+xhr.send()
+
+
+//strip response to activeMinutes components --> _submission
 
 //send to smart contract "settle(_submission)"
 
