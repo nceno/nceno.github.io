7471b9a4d75c69fd5fc786ecf3ab6fb08c456494
nance2uiuc <nance2uiuc@gmail.com>
2019-01-11 23:43:01 +0800

    asdf

--- computation/call _function.js
+++ computation/call _function.js
@@ -1,64 +1,24 @@
-//calling a function
-$("#BUTTON").click(function() {
-  //var usdStake = ($("#stake").val()-1.05)*0.0057;
-  CONTRACT.FUNCTION(
-    $("#ELEMENT ID").val(),
-    GLOBALVAR,
-    ('TXTPADDING'+$("#ELEMENT ID").val()), 
-    //$("#stake").val(),
-    //web3.toWei(usdStake, "ether"),
-    {from: web3.eth.accounts[0], gas: GASLIMIT, gasPrice: XX000000000, value: web3.toWei(usdStake, "ether")},
+//creating a goal
+//Date.now()/1000,
+var usdStake = ($("#stakeDD").val()-2)*0.0048;
+$("#hostBtn").click(function() {
+  Nceno.methods.createGoal(
+    web3.utils.randomHex(32),
+    $("#activeMinsDD").val(),
+    web3.utils.toWei(usdStake, "ether"),
+    $("#sesPerWkDD").val(),
+    $("#wksDD").val(),
+    $("#datetimepicker1").val(),
+    userID
+  )
+  .send({from: web3.eth.defaultAccount, gas: 5000000, gasPrice: 15000000000},
     function(error, result) {
       if (!error){
-        $("#createBtn").hide();
-        $("#loader").show();
-        $("#insTrans").html(result.blockHash);
-        console.log(result);
-      }
-      else
-      console.error(error);
-    }
-  )
-}); 
-
-
-
-//creating a competitor account
-$("#makeAcctBtn").click(function() {
-  Nceno.createCompetitor(
-    userID,
-    $("#wearable").val(),
-    $("#name").val(),
-    $("#email").val()
-  )
-  .send({from: web3.eth.accounts[0], gas: 8000000, gasPrice: 21000000000},
-    function(error, result) {
-      if (!error){
-        $("#makeAcctBtn").hide();
-        $("#loader").show();
-        $("#insTrans").html(result.blockHash);
+        $("#hostBtn").hide();
         console.log(result);
       }
       else
       console.error(error);
     }
   );
-}); 
-
-
-//waiting for it to be mined
-var profileCreatedEvent = Nceno.events.profileCreated({},'latest');
-        profileCreatedEvent.watch(function(error, result){
-            if (result)
-                {
-                  if (result.blockHash != $("#insTrans").html()) //when the creation txn is mined, and goal spawned
-                    console.log(result.blockHash);
-                    //loader hide
-                    $("#loader").hide();
-                    //echo goal creation
-                    $("#profCre").html('Success!'+ web3.toAscii(result.args._name) );
-                } else {
-                    $("#loader").hide();
-                    console.log(error);
-                  }
-        });
+});
