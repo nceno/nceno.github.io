b7ed861e8f84b14855aeca4badd2300a82bfa8b5
nance2uiuc <nance2uiuc@gmail.com>
2019-01-14 17:38:20 +0800

    asefe

--- computation/call _function.js
+++ computation/call _function.js
@@ -24,18 +24,18 @@ $("#hostBtn").click(function() {
 
 var populated = false;
 function makeList(){
-    //makes a list of active goals for a user
-    if(populated = false){
-      var count;
-      Nceno.methods.profileOf[userID].goalTotal.call({from: web3.eth.defaultAccount}, function(error, result){count = result});
-      var i = 0;
-      var goals = new Array();
-      console.log(count, web3.eth.defaultAccount); //debug
-      for (i = 0; i < count; i++){
-        Nceno.methods.profileOf[userID].goalAt[i].goalID.call({from: web3.eth.defaultAccount}, function(error, result){goals[i] = result});
-        console.log(goals[i]); //debug
+  //makes a list of active goals for a user
+  if(populated = false){ 
+    var i = 0;
+    var goals = new Array();
+
+    for (i = 0; i < 3; i++){
+      Nceno.methods.getActiveGoal(userID, i).call({from: web3.eth.defaultAccount}, function(error, result){
+        goals[i] = result;
+        console.log(goals[i]);
         $("#chIDtools").append('<option>'+ goals[i] +'</option>');
-      }
-      populated = true;
+      });    
     }
+    populated = true;
   }
+}
