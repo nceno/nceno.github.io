d2b3f1549d2c6d26c019e82a73a8ecbfcac6ae7e
nance2uiuc <nance2uiuc@gmail.com>
2019-01-25 20:04:39 +0800

    asdf

--- logic.js
+++ logic.js
@@ -822,7 +822,7 @@
                 console.log(goals[i]);
               }
                 //$("#chIDtools").append("<option>"+ goals[i].slice(0, 12) +"</option>");
-                $("#op1").html(goals[1].slice(0, 12));
+                $("#op1").html("my nigga");
                 $("#op2").html(goals[2].slice(0, 12));
                 $("#op3").html(goals[3].slice(0, 12));
                 $("#op4").html(goals[4].slice(0, 12));
