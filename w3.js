db4db6da6306a8dd8c9935c47823a2c06e1cfc2e
nance2uiuc <nance2uiuc@gmail.com>
2019-01-31 21:01:20 +0800

    asdf

---
+++ w3.js
@@ -0,0 +1,13 @@
+//$("#loader").hide();
+var PortisProvider = window.Portis.PortisProvider;
+// Check if Web3 has been injected by the browser (Mist/MetaMask)
+if (typeof web3 !== 'undefined') {
+  // Use Mist/MetaMask's provider
+  web3 = new Web3(web3.currentProvider);
+} else {
+    // Fallback - use Portis
+    web3 = new Web3(new PortisProvider({
+      apiKey: "332bfe3ea28174fa515d478e23a1b31c",
+      network: 'rinkeby'
+    }));
+}
