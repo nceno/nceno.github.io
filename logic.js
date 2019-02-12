f4e4d4cbb65e9ef8c70cf97b3383bfb5629cb2bb
nance2uiuc <nance2uiuc@gmail.com>
2019-02-12 16:44:06 +0800

    asd

--- logic.js
+++ logic.js
@@ -156,48 +156,49 @@ var populated = false;
 function makeList(){
   //makes a list of active goals for a user
   if(populated === false){
-    $("#goalCategories").selectric(); 
+    $("#goalCategories").selectric();
+    //$("#chIDtoolsActive").selectric();
+    //$("#chIDtoolsCompleted").selectric(); 
     var i = 0;
     var j = 0;
     var k = 0;
     var goals1 = new Array();
     var goals2 = new Array();
     var goals3 = new Array();
-
-    $("#goalCategories").append('<optgroup id="upcomingGoals" label="Upcoming:"> </optgroup>');
+    //implement this method
     for (i = 0; i < 15; i++){
       Nceno.methods.getUpcomingGoal(userID, i).call({from: web3.eth.defaultAccount}, function(error, result){
         if(result != 0x0000000000000000000000000000000000000000000000000000000000000000 && result != undefined){
           goals1[i] = result;
           console.log(goals1[i]);
           $("#goalCategories").append('<option>'+ goals1[i].slice(0, 8) +'</option>');
+          //$('#goalCategories').selectric('refresh');
         }
       });    
     }
 
-    $("#goalCategories").append('<optgroup id="activeGoals" label="Active:"> </optgroup>');
     for (j = 0; j < 15; j++){
       Nceno.methods.getActiveGoal(userID, j).call({from: web3.eth.defaultAccount}, function(error, result){
         if(result != 0x0000000000000000000000000000000000000000000000000000000000000000 && result != undefined){
           goals2[j] = result;
           console.log(goals2[j]);
-          $("#goalCategories").append('<option>'+ goals2[j].slice(0, 8) +'</option>');
+          $("#activeGoals").append('<option>'+ goals2[j].slice(0, 8) +'</option>');
+          //$('#goalCategories').selectric('refresh');
         }
       });    
     }
-
-    $("#goalCategories").append('optgroup id="completedGoals" label="Completed:"> </optgroup>');
+    //implement this method 
     for (k = 0; k < 15; k++){
       Nceno.methods.getCompletedGoal(userID, k).call({from: web3.eth.defaultAccount}, function(error, result){
         if(result != 0x0000000000000000000000000000000000000000000000000000000000000000 && result != undefined){
           goals3[k] = result;
           console.log(goals3[k]);
-          $("#goalCategories").append('<option>'+ goals3[k].slice(0, 8) +'</option>');          
+          $("#completedGoals").append('<option>'+ goals3[k].slice(0, 8) +'</option>');
+          $('#goalCategories').selectric('refresh');
         }
       });    
     }
-    $('#goalCategories').selectric('refresh');
-    //populated = true;
+    populated = true;
   }
 }
 
