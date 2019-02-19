41eaeedba74c85b78b8af3c16ecda04fe3e57d7a
nance2uiuc <nance2uiuc@gmail.com>
2019-02-20 00:31:29 +0800

    asdf

--- logic.js
+++ logic.js
@@ -1,7 +1,8 @@
 //datepicker initializer
 var first = new Date();
 first.setDate(first.getDate() + 1);
-$('[data-toggle="datepicker"]').datepicker({'autoHide': true, 'startDate': first});
+//$('[data-toggle="datepicker"]').datepicker({'autoHide': true, 'startDate': first});
+$('[data-toggle="datepicker"]').datepicker({'autoHide': true});
 $("#time").click(function(){
   var time = new Date($("#dateChoice").val()).getTime() / 1000;
   console.log(time);
