7bd95a5e9d7cb1a207fe2d423eccdce6cba6eb51
nance2uiuc <nance2uiuc@gmail.com>
2019-02-20 21:46:14 +0800

    asdf

--- logic.js
+++ logic.js
@@ -253,7 +253,7 @@ function selectedChallenge(){
         if (!error){
           //echo challenge
           var tstamp = new Date(result[4]*1000);
-          var buyin = Math.floor(result[1]*result[5]/1000000000000000000);
+          var buyin = result[1]*result[5]/1000000000000000000;
           $("#echStake").html("$"+buyin);
           $("#echWks").html(result[3]+" wks");
           $("#echSes").html(result[2]+"x/wk");
