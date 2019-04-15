d65df83ce0bbc0aba74a651b7e31ed76e657d3e8
nance2uiuc <nance2uiuc@gmail.com>
2019-04-15 17:57:39 +0800

    asdg

--- logic.js
+++ logic.js
@@ -398,7 +398,7 @@ function selectedChallenge(){
   $('#goalCategories').selectric().on('change', function() {
     var goalid = web3.utils.padRight($('#goalCategories').val(),34);
     
-    $("rows").empty();
+    $("#rows").empty();
     
     Nceno.methods.getGoalParams(goalid)
     .call({from: web3.eth.defaultAccount},
@@ -523,26 +523,27 @@ function search(){
 }
 
 //populates the challenges starting soon table
-
+var browsePopulated = false;
 async function browse(){
-  $("startingSoon").empty();
+  $("#startingSoon").empty();
+  if(browsePopulated=false){
+    for (let i = 0; i < 20; i++){
+      var result = await Nceno.methods.getFutureGoal(i).call({from: web3.eth.defaultAccount});
+        if(result[0] != 0x0000000000000000000000000000000000000000000000000000000000000000 && result[0] != undefined){
+          //list it in the table
+          var tstamp = new Date(result[5]*1000);
+          var buyin = Math.round(result[2]/100000000000000000000);
 
-  for (let i = 0; i < 20; i++){
-    var result = await Nceno.methods.getFutureGoal(i).call({from: web3.eth.defaultAccount});
-    if(result[0] != 0x0000000000000000000000000000000000000000000000000000000000000000 && result[0] != undefined){
-      //list it in the table
-      var tstamp = new Date(result[5]*1000);
-      var buyin = Math.round(result[2]/100000000000000000000);
-
-      $("startingSoon").after('<tr><td>$'+buyin+
-        '</td><td>'+result[4]+
-        ' wks</td><td>'+result[3]+
-        ' x/wk</td><td>'+result[1]+
-        ' min</td><td>'+result[6]+
-        ' </td><td>'+tstamp.toDateString()+
-        '</td><td><button type="button" onclick="setGoalID('+result[0]+')" id="soonJoin" class="btn btn-primary px-1 py-0 ml-0 mt-0" data-toggle="modal" data-target="#popupSoonJoin" data-whatever="@mdo">Join</button></td></tr>'
-      );
-    }   
+          $("#startingSoon").after('<tr><td>$'+buyin+
+            '</td><td>'+result[4]+
+            ' wks</td><td>'+result[3]+
+            ' x/wk</td><td>'+result[1]+
+            ' min</td><td>'+result[6]+
+            ' </td><td>'+tstamp.toDateString()+
+            '</td><td><button type="button" onclick="setGoalID('+result[0]+')" id="soonJoin" class="btn btn-primary px-1 py-0 ml-0 mt-0" data-toggle="modal" data-target="#popupSoonJoin" data-whatever="@mdo">Join</button></td></tr>');
+        }   
+    }
+    browsePopulated = true;
   }
 }
 
