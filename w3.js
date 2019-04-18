ac33ad341c386577cd16ccb4caf5763548c04a82
nance2uiuc <nance2uiuc@gmail.com>
2019-04-18 20:17:07 +0800

    sfhge

--- w3.js
+++ w3.js
@@ -1,13 +1,20 @@
 
-var PortisProvider = window.Portis.PortisProvider;
+//var PortisProvider = window.Portis.PortisProvider;
+//const portis = new Portis(...)
+
 // Check if Web3 has been injected by the browser (Mist/MetaMask)
 if (typeof web3 !== 'undefined') {
   // Use Mist/MetaMask's provider
   web3 = new Web3(web3.currentProvider);
 } else {
     // Fallback - use Portis
-    web3 = new Web3(new PortisProvider({
+    /*web3 = new Web3(new PortisProvider({
       apiKey: "332bfe3ea28174fa515d478e23a1b31c",
       network: 'ropsten'
-    }));
+    }));*/
+
+    const portis = new Portis('67f0b194-14fb-4210-8535-d629eeb666b6', 'ropsten');
+	const web3 = new Web3(portis.provider);
 }
+
+
