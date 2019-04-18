60c7c9100736f31fae44f9e4f9fa1a16b88bda25
nance2uiuc <nance2uiuc@gmail.com>
2019-04-18 20:38:27 +0800

    qbefb

--- w3.js
+++ w3.js
@@ -1,18 +1,17 @@
 
-
+//var PortisProvider = window.Portis.PortisProvider;
 const portis = new Portis('67f0b194-14fb-4210-8535-d629eeb666b6', 'ropsten');
 const web3 = new Web3(portis.provider);
 
-//var PortisProvider = window.Portis.PortisProvider;
-
 // Check if Web3 has been injected by the browser (Mist/MetaMask)
-/*if (typeof web3 !== 'undefined') {
+
+//if (typeof web3 !== 'undefined') {
   // Use Mist/MetaMask's provider
-  web3 = new Web3(web3.currentProvider);
-} else {
+  //web3 = new Web3(web3.currentProvider);
+//} else {
 	// Fallback - use Portis
     /*web3 = new Web3(new PortisProvider({
       apiKey: "332bfe3ea28174fa515d478e23a1b31c",
       network: 'ropsten'
     }));*/
-}*/
+//}
