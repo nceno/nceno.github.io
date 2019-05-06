14a4f477266c46424fbc15b8293fd93718870b88
nance2uiuc <nance2uiuc@gmail.com>
2019-05-06 23:23:42 +0800

    styed

--- chartdata.js
+++ chartdata.js
@@ -1,4 +1,4 @@
-var increasing = [0,1,2,3,4,5,6,7,8,9,10,11,12];
+
 //chart1
 //<script>
 var config1 = {
@@ -9,7 +9,7 @@ var config1 = {
         label: 'Workouts you completed',
         yAxisID: 'A',
         //data: [3, 3, 2, 3, 1, 0, 3, 1, 3, 3, 2, 3],
-        data: increasing,
+        data: successesWk,
         backgroundColor: '#ccff00',
         borderColor: '#ccff00',
         fill: true
@@ -17,15 +17,15 @@ var config1 = {
         //line data
         label: 'Your cumulative % stake earned back',
         yAxisID: 'B',
-        data: [2, 7, 12, 26, 26, 30, 45, 45, 78, 85, 85, 90],
-        //data: 
+        //data: [2, 7, 12, 26, 26, 30, 45, 45, 78, 85, 85, 90],
+        data: roi,
         backgroundColor: '#f442b3',
         borderColor: '#f442b3',
         fill: false,
         type: 'line'
     }],
-    labels: ['Week 1', 'Week 2', 'Week 3', 'Week 4', 'Week 5', 'Week 6', 'Week 7', 'Week 8', 'Week 9', 'Week 10', 'Week 11', 'Week 12']
-    //labels: xaxis
+    //labels: ['Week 1', 'Week 2', 'Week 3', 'Week 4', 'Week 5', 'Week 6', 'Week 7', 'Week 8', 'Week 9', 'Week 10', 'Week 11', 'Week 12']
+    labels: xaxis
   },
   
 
@@ -100,7 +100,8 @@ var config2 = {
         //bar data
         label: '% stake locked up',
         yAxisID: 'A',
-        data: [2, 5, 7, 5, 9, 15, 10, 3, 8, 18, 11, 7],
+        //data: [2, 5, 7, 5, 9, 15, 10, 3, 8, 18, 11, 7],
+        data: lockedPercentWk,
         backgroundColor: '#ccff00',
         borderColor: '#ccff00',
         fill: true
@@ -108,13 +109,15 @@ var config2 = {
         //line
         label: '% Competitors finished the week',
         yAxisID: 'B',
-        data: [90, 95, 60, 40, 55, 70, 30, 45, 40, 30, 20, 43],
+        //data: [90, 95, 60, 40, 55, 70, 30, 45, 40, 30, 20, 43],
+        data: finishers,
         type: 'line',
         backgroundColor: '#f442b3',
         borderColor: '#f442b3',
         fill: false
     }],
-    labels: ['Week 1', 'Week 2', 'Week 3', 'Week 4', 'Week 5', 'Week 6', 'Week 7', 'Week 8', 'Week 9', 'Week 10', 'Week 11', 'Week 12']
+    //labels: ['Week 1', 'Week 2', 'Week 3', 'Week 4', 'Week 5', 'Week 6', 'Week 7', 'Week 8', 'Week 9', 'Week 10', 'Week 11', 'Week 12']
+    labels: xaxis
   },
   
 
