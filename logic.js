63c4f724f28d9c5a3abc2d2db7e7465ec52d0722
nance2uiuc <nance2uiuc@gmail.com>
2019-05-29 23:14:53 +0800

    wedfq

--- logic.js
+++ logic.js
@@ -1587,7 +1587,7 @@ function getToken(){
 //var stravaMins;
 //var avgHR;
 var placeholderDate = new Date();
-placeholderDate.setDate(placeholderDate.getDate() - 1); //can change "1" day to "20" days for testing.
+placeholderDate.setDate(placeholderDate.getDate() - 100); //can change "1" day to "20" days for testing.
 var yesterday =parseInt(parseInt(placeholderDate.getTime())/1000);
 var nowDate = parseInt(parseInt(new Date().getTime())/1000);
 
