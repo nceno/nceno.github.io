d573a0f694c088253e547f458ccaffa19115c108
nance2uiuc <nance2uiuc@gmail.com>
2019-05-30 16:03:17 +0800

    hwr

--- logic.js
+++ logic.js
@@ -1527,6 +1527,8 @@ function getActivities(){
           cleaned[i] = [data[i].id, data[i].average_heartrate, data[i].elapsed_time/60];
           //cleaned.push([data[i].id, data[i].average_heartrate, data[i].moving_time/60]);
           console.log("added: ["+cleaned[i]+"]");
+          console.log("first one is: ["+cleaned[0]+"]");
+          //console.log("and first part of that is: ["+cleaned[0]+"]");
         }
       }
       console.log("cleaned length is: "+cleaned.length);
