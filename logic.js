2d2a4184855492ce1f1e3252a2e893d892e03a5a
nance2uiuc <nance2uiuc@gmail.com>
2019-06-04 11:00:12 +0800

    gew

--- logic.js
+++ logic.js
@@ -1,10 +1,8 @@
 $('#circle').circleProgress({
-  value: 0.75,
-  size: 80,
-  fill: {
-    gradient: ["red", "orange"]
-  }
-});
+    value: 0.6
+  }).on('circle-animation-progress', function(event, progress) {
+    $(this).find('strong').html(Math.round(100 * progress) + '<i>%</i>');
+  });
 
 
 //get test ether that is stored in the contract
