0965294ec2518a38d06fdf7977a094d8af799851
nance2uiuc <nance2uiuc@gmail.com>
2019-09-07 20:11:04 +0800

    gqer

--- debug_logic.js
+++ debug_logic.js
@@ -1433,7 +1433,7 @@ window.onload = function() {
   });
 
   //delays extraction of the fitbit creds until the user has authed.
-  if (window.location.href != 'https://www.nceno.app/app.html'){
+  if (window.location.href != 'https://www.nceno.app/app.html' && window.location.href != 'https://www.nceno.app/app'){
     $("#stravaBtn").hide();
     $("#stravaOk").html("Proceed to step 2...")
     $("#stravaOk").show();
