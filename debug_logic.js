415e7e370f60c559cb2363b1167d53c76044d3db
nance2uiuc <nance2uiuc@gmail.com>
2019-09-17 22:22:54 +0800

    aegqa

--- debug_logic.js
+++ debug_logic.js
@@ -1727,8 +1727,8 @@ function getRawHR(){
     if (this.readyState === 4) {
       console.log(this.responseText);
       var resp = JSON.parse(xhr.responseText);
-      var hr = resp.heartrate[0];
-      var tm = resp.time[0];
+      var hr = resp.heartrate[0].data;
+      var tm = resp.time[0].data;
       var dataString='';
       for(let u=0; u<hr.length; u++){
         data[u]=[time[u],hr[u]];
