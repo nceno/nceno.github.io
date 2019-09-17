530d4e49e83e5c8524ef17e98459c4ee86fe22e8
nance2uiuc <nance2uiuc@gmail.com>
2019-09-17 22:21:51 +0800

    gqe

--- debug_logic.js
+++ debug_logic.js
@@ -1740,7 +1740,7 @@ function getRawHR(){
       console.log("none"); 
     } 
   });
-  xhr.open("GET", 'https://www.strava.com/api/v3/activities/2716096931/streams?keys=heartrate,time&series_type=time&key_by_type=true');
+  xhr.open("GET", 'https://www.strava.com/api/v3/activities/2712826552/streams?keys=heartrate,time&series_type=time&key_by_type=true');
   xhr.setRequestHeader("Authorization", 'Bearer ' + access_token);
   xhr.send(stuff);
 }
