e16344b47afbd7ddc0935cd6c3311d95b7c68829
nance2uiuc <nance2uiuc@gmail.com>
2019-09-17 13:39:07 +0800

    rhw

--- debug_logic.js
+++ debug_logic.js
@@ -1728,7 +1728,7 @@ function getRawHR(){
     else{
       console.log("none"); 
     } 
-  }
+  });
   xhr.open("GET", 'https://www.strava.com/api/v3/activities/2715300973/streams?keys=heartrate&key_by_type=true');
   xhr.setRequestHeader("Authorization", 'Bearer ' + access_token);
   xhr.send(stuff);
