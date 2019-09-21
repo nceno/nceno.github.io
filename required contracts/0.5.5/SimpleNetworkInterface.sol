475a7a899aad403db0a685d31a787906aa433916
nance2uiuc <nance2uiuc@gmail.com>
2019-09-21 21:02:27 +0800

    eqr

---
+++ required contracts/0.5.5/SimpleNetworkInterface.sol
@@ -0,0 +1,12 @@
+pragma solidity >=0.4.24;
+
+
+import "./ERC20Interface.sol";
+
+
+/// @title simple interface for Kyber Network 
+interface SimpleNetworkInterface {
+    function swapTokenToToken(ERC20 src, uint srcAmount, ERC20 dest, uint minConversionRate) external returns(uint);
+    function swapEtherToToken(ERC20 token, uint minConversionRate) external payable returns(uint);
+    function swapTokenToEther(ERC20 token, uint srcAmount, uint minConversionRate) external returns(uint);
+}
