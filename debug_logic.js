dd113d21d0ae2821c3532e05e6340fd645b4de6f
nance2uiuc <nance2uiuc@gmail.com>
2019-10-27 19:07:16 +0800

    rgq

--- debug_logic.js
+++ debug_logic.js
@@ -1585,7 +1585,7 @@ function getToken(){
       //console.log(uniqueUserString);
 
       console.log("Nceno User ID: "+stravaID+"   Nceno Email: "+portisEmail+"   Wallet address: "+web3.eth.defaultAccount);
-      $("#athleteInfo").html('<p>"Nceno User ID: "'+stravaID+'"   <br>Nceno Email: "'+portisEmail+'"   <br>Wallet address: "'+web3.eth.defaultAccount.slice(0, 22)+' '+web3.eth.defaultAccount.slice(23, 42)'</p>');
+      $("#athleteInfo").html('<p>"Nceno User ID: "'+stravaID+'"   <br>Nceno Email: "'+portisEmail+'"   <br>Wallet address: "'+web3.eth.defaultAccount.slice(0, 22)+' '+web3.eth.defaultAccount.slice(23, 42)+'</p>');
       
       Nceno.methods.userExists(stravaID
       )
