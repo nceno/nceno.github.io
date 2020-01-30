ce3400db123ad4a6b0a52985d5a1601d7232da9b
nance2uiuc <nance2uiuc@gmail.com>
2020-01-31 01:24:21 +0800

    4tqe

--- brands/brands_demo_logic.js
+++ brands/brands_demo_logic.js
@@ -1127,8 +1127,8 @@ function getActivities(){
 $("#loaderGlobal").hide();
 $("#openWalletGlobal2").hide();
 //initialize portis
-const portis = new Portis('67f0b194-14fb-4210-8535-d629eeb666b6', 'rinkeby', { gasRelay: true, scope: ['email'] });
-const web3 = new Web3(portis.provider);
+//const portis = new Portis('67f0b194-14fb-4210-8535-d629eeb666b6', 'rinkeby', { gasRelay: true, scope: ['email'] });
+//const web3 = new Web3(portis.provider);
 
 
 
