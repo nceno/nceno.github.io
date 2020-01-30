0515a1f568d0c45e61a304247052bc4e6caec44d
nance2uiuc <nance2uiuc@gmail.com>
2020-01-31 00:46:24 +0800

    gae

--- login_workplace.js
+++ login_workplace.js
@@ -26,6 +26,7 @@ window.onload = function() {
 	//case 1- if you're missing everything,
 	if(Cookies.get('access_token') == 'undefined' || Cookies.get('stravaID') == 'undefined'){ 
 		//&& Cookies.get('stravaUsername') == 'undefined'){
+    console.log("doing case 1: missing everything...");
 		$("#stravaBtnGlobal").show();
 		//$("#portisBtnGlobal").show();
 		$("#userPrompt").html('');
@@ -57,6 +58,7 @@ window.onload = function() {
 	
 	//case 2- missing portis only
 	else if( Cookies.get('stravaUsername') == 'undefined'){
+    console.log("doing case 2: missing portis only...");
 		$("#stravaBtnGlobal").hide();
     $("#workplacePrompt").html('You need to log in! <a href="www.nceno.app/workplace.html">click here</a>');
 		//$("#portisBtnGlobal").show();
@@ -68,6 +70,7 @@ window.onload = function() {
 	//case 4- nothing missing
 		//-----disable this block when testing.------
 	else if(Cookies.get('access_token') != 'undefined' && Cookies.get('stravaID') != 'undefined' && Cookies.get('stravaUsername') != 'undefined'){
+    console.log("doing case 3: missing nothing...");
 		$("#stravaBtnGlobal").hide();
 		$("#openWalletGlobal").show();
 		//$("#portisBtnGlobal").hide();
