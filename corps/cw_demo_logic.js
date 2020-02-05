8f8379ae84d1da3d1d7fed4d3ab4b2a5be1fd328
nance2uiuc <nance2uiuc@gmail.com>
2020-02-06 01:39:23 +0800

    efwq

--- corps/cw_demo_logic.js
+++ corps/cw_demo_logic.js
@@ -793,7 +793,7 @@ if(Cookies.get('userWallet') != undefined){
 
 var code;
 window.onload = function(){
-  makeWorkoutPage();
+
   //$("#brandsPrompt").html('<p>'+Cookies.get('access_token')+'</p><br><p>'+Cookies.get('stravaID')+'</p><br><p>'+Cookies.get('stravaUsername')+'</p><br><p>'+Cookies.get('userWallet')+'</p>');
   //case 1- if you're missing everything,
   if(Cookies.get('access_token') == undefined || Cookies.get('stravaID') == undefined ){ 
@@ -843,6 +843,7 @@ window.onload = function(){
     $("#userPrompt").html('<h5><font style="color:white;">Connection successful. Welcome, </font>'+Cookies.get('stravaUsername')+'</h5>');
     $("#brandsPrompt").html('<p><font style="color:white;">Connection successful. <br>Welcome, </font>'+Cookies.get('stravaUsername')+'</p>');
     updateNonce();
+    makeWorkoutPage();
   }
 
   //we can only use window.onload once... so move the slider initialization here
