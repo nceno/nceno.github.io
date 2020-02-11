ad60bb3e116b83c7da4c0607bf68d62f789779a2
nance2uiuc <nance2uiuc@gmail.com>
2020-02-11 20:19:33 +0800

    dsfg

--- corps/cw_demo_logic.js
+++ corps/cw_demo_logic.js
@@ -1,4 +1,4 @@
-console.log("hshssss");
+console.log("i674k4");
 //const portis = new Portis('67f0b194-14fb-4210-8535-d629eeb666b6', 'rinkeby', { gasRelay: true, scope: ['email'] });
 //const web3 = new Web3(portis.provider);
 
@@ -38,9 +38,9 @@ function makeWorkoutPage(){
         var kmReward = parseInt(resultA[6]); //per km
 
         $('#me').empty();
-        for(var j=0; j<compcount; j++){
+        /*for(var j=0; j<compcount; j++){
           $('#entry'+j).remove();
-        }
+        }*/
 
         //---get other players
         for(var i= 0; i<compcount; i++){
@@ -113,12 +113,12 @@ function makeWorkoutPage(){
 
                       }
                       //only after if there isn't already an element of the same name
-                      else if(! $('#entry'+i).length){
+                      else if(! $('#'+playerName).length){
                         //.after following entries
                         console.log(playerName+" is player "+i);
                         //$('#entry'+ (i-1)).after(
                         $('#startList').after(
-                          '<div id="entry'+i+'" class="col-12 mt-2"><h4 class="progress-title">'  +playerName+ '<font style="color:#ccff00;"> +' +theirReward+' '+TOKENSYMBOL+ '</font> / <font style="color:#f442b3;">' +theirKms+ 'km + '+theirMins+'mins</font></h4><div class="progress-item"><div class="progress"><div class="progress-bar bg-blue" role="progressbar" style="width:' +theirProgress+ '%;" aria-valuenow="' +theirProgress+ '" aria-valuemin="0" aria-valuemax="100"><span><img height="40" width="40" src="../app/assets/images/'+avatar+'.png"> </span></div></div></div></div>'
+                          '<div id="'+playerName+'" class="col-12 mt-2"><h4 class="progress-title">'  +playerName+ '<font style="color:#ccff00;"> +' +theirReward+' '+TOKENSYMBOL+ '</font> / <font style="color:#f442b3;">' +theirKms+ 'km + '+theirMins+'mins</font></h4><div class="progress-item"><div class="progress"><div class="progress-bar bg-blue" role="progressbar" style="width:' +theirProgress+ '%;" aria-valuenow="' +theirProgress+ '" aria-valuemin="0" aria-valuemax="100"><span><img height="40" width="40" src="../app/assets/images/'+avatar+'.png"> </span></div></div></div></div>'
                         );
                       }                      
                     }
