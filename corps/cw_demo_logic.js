ed6b95eac6b7dc701a21afee82e87ecd77083b6f
nance2uiuc <nance2uiuc@gmail.com>
2020-02-11 19:06:14 +0800

    dfa

--- corps/cw_demo_logic.js
+++ corps/cw_demo_logic.js
@@ -1,4 +1,4 @@
-//rwr454
+//rwdsf
 //const portis = new Portis('67f0b194-14fb-4210-8535-d629eeb666b6', 'rinkeby', { gasRelay: true, scope: ['email'] });
 //const web3 = new Web3(portis.provider);
 
@@ -95,7 +95,7 @@ async function makeWorkoutPage(){
 
                       if(playerID == Cookies.get('stravaID')){
                         //post to top if it's me
-                        console.log("it's me..."+playerName+"...i= "+i);
+                        console.log("I am"+playerName+". player "+i);
                         //if(! $('#me').length){
                           $("#me").prepend(
                             '<h4 class="progress-title">'  +playerName+ '<font style="color:#ccff00;"> +' +theirReward+' '+TOKENSYMBOL+ '</font> / <font style="color:#f442b3;">' +theirKms+ 'km + '+theirMins+'mins</font></h4><div class="progress-item"><div class="progress"><div class="progress-bar bg-blue" role="progressbar" style="width:' +theirProgress+ '%;" aria-valuenow="' +theirProgress+ '" aria-valuemin="0" aria-valuemax="100"><span><img height="40" width="40" src="../app/assets/images/'+avatar+'.png"> </span></div></div>'
