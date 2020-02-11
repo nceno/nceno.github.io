62bc700ea4cdcd778ac2e066f9b340b4b711e249
nance2uiuc <nance2uiuc@gmail.com>
2020-02-11 18:43:58 +0800

    dsaf

--- corps/cw_demo_logic.js
+++ corps/cw_demo_logic.js
@@ -1,4 +1,4 @@
-//54656
+//yyyy
 //const portis = new Portis('67f0b194-14fb-4210-8535-d629eeb666b6', 'rinkeby', { gasRelay: true, scope: ['email'] });
 //const web3 = new Web3(portis.provider);
 
@@ -39,6 +39,8 @@ async function makeWorkoutPage(){
 
         //---get other players
         for(var i= 0; i<compcount; i++){
+          console.log("----------------------- next iteration----------");
+          console.log("i= "+i);
           await NcenoBrands.methods.getIndexedPlayerID(_goalID, i)
           .call({from: Cookies.get('userWallet')},
             async function(error, result) {
@@ -112,8 +114,7 @@ async function makeWorkoutPage(){
                         $('#startList').after(
                           '<div id="entry'+i+'" class="col-12 mt-2"><h4 class="progress-title">'  +playerName+ '<font style="color:#ccff00;"> +' +theirReward+' '+TOKENSYMBOL+ '</font> / <font style="color:#f442b3;">' +theirKms+ 'km + '+theirMins+'mins</font></h4><div class="progress-item"><div class="progress"><div class="progress-bar bg-blue" role="progressbar" style="width:' +theirProgress+ '%;" aria-valuenow="' +theirProgress+ '" aria-valuemin="0" aria-valuemax="100"><span><img height="40" width="40" src="../app/assets/images/'+avatar+'.png"> </span></div></div></div></div>'
                         );
-                      }
-                      await console.log("i= "+i);
+                      }                      
                     }
                     else{
                       console.error(error);
