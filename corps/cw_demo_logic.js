27e861ec2535012cd7a69c1ca21557cc2e3cfda5
nance2uiuc <nance2uiuc@gmail.com>
2020-02-16 20:39:27 +0800

    sdfa

--- corps/cw_demo_logic.js
+++ corps/cw_demo_logic.js
@@ -1,4 +1,4 @@
-console.log("regw");
+console.log("456fgdf");
 //const portis = new Portis('67f0b194-14fb-4210-8535-d629eeb666b6', 'rinkeby', { gasRelay: true, scope: ['email'] });
 //const web3 = new Web3(portis.provider);
 
@@ -35,7 +35,7 @@ function makeSpendPage(){
           .call({from: Cookies.get('userWallet'), nonce: correctNonce},
             async function(error, result) {
               if (!error){
-                await $("#orderHistory").append('<tr><td>'+result[0]+'</td><td>'+result[1]+'</td><td>'+result[2]+'</td><td>'+Date(result[3]).toDateString()+'</td></tr>');
+                await if(! $('#order'+result[0]).length) $("#orderHistory").append('<tr id="order'+result[0]+'"><td>'+result[0]+'</td><td>'+result[1]+'</td><td>'+result[2]+'</td><td>'+Date(result[3]).toDateString()+'</td></tr>');
               }
               else
               console.error(error);
