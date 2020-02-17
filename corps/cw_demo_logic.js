365ae252106011b73791dec0c588c2c38b415566
nance2uiuc <nance2uiuc@gmail.com>
2020-02-17 21:53:11 +0800

    dsf

--- corps/cw_demo_logic.js
+++ corps/cw_demo_logic.js
@@ -176,10 +176,14 @@ async function makeOrdersPage(){
                 //var statusCode;
                 switch(await result2[4]) {
                   case "0":
-                    var statusCode = "new";
+                    //var statusCode = "new";
+                    if(! $('#co'+result2[5]).length) $("#orderList").append('<tr id="co'+result2[5]+'"><td id = "status'+result2[5]+'" data-toggle="modal" data-target="#refundModal" onclick="setRefTarget('+"'"+result2[5]+"'"+');" data-whatever="@mdo" >new</td><td style="color:#ccff00;" id = "order'+result2[5]+'">'+result2[5]+'</td><td id = "name'+result2[5]+'">'+result2[1]+'</td><td id = "item'+result2[5]+'">'+result2[0]+'</td><td id = "cost'+result2[5]+'" >'+result2[2]+'</td><td id = "date'+result2[5]+'">'+new Date(result2[3]*1000).toDateString()+'</td></tr>');
+
                     break;
                   case "1":
-                    var statusCode = "complete";
+                    //var statusCode = "complete";
+                    if(! $('#co'+result2[5]).length) $("#orderList").append('<tr id="co'+result2[5]+'"><td id = "status'+result2[5]+'" data-toggle="modal" data-target="#refundModal" onclick="setRefTarget('+"'"+result2[5]+"'"+');" data-whatever="@mdo" >complete</td><td style="color:#ccff00;" id = "order'+result2[5]+'">'+result2[5]+'</td><td id = "name'+result2[5]+'">'+result2[1]+'</td><td id = "item'+result2[5]+'">'+result2[0]+'</td><td id = "cost'+result2[5]+'" >'+result2[2]+'</td><td id = "date'+result2[5]+'">'+new Date(result2[3]*1000).toDateString()+'</td></tr>');
+
                     $('#order'+result2[5]).css({color: "#333"});
                     $('#name'+result2[5]).css({color: "#333"});
                     $('#item'+result2[5]).css({color: "#333"});
@@ -190,7 +194,9 @@ async function makeOrdersPage(){
                     
                     break;
                   case "2":
-                    var statusCode = "refunded";
+                    //var statusCode = "refunded";
+                    if(! $('#co'+result2[5]).length) $("#orderList").append('<tr id="co'+result2[5]+'"><td id = "status'+result2[5]+'" data-toggle="modal" data-target="#refundModal" onclick="setRefTarget('+"'"+result2[5]+"'"+');" data-whatever="@mdo" >refunded</td><td style="color:#ccff00;" id = "order'+result2[5]+'">'+result2[5]+'</td><td id = "name'+result2[5]+'">'+result2[1]+'</td><td id = "item'+result2[5]+'">'+result2[0]+'</td><td id = "cost'+result2[5]+'" >'+result2[2]+'</td><td id = "date'+result2[5]+'">'+new Date(result2[3]*1000).toDateString()+'</td></tr>');
+
                     $('#order'+result2[5]).css({color: "#333"});
                     $('#name'+result2[5]).css({color: "#333"});
                     $('#item'+result2[5]).css({color: "#333"});
