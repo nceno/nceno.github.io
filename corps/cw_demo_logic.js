a1993875196bf1bb501a05bcba2a34cbf1b05866
nance2uiuc <nance2uiuc@gmail.com>
2020-02-17 18:57:34 +0800

    efawe

--- corps/cw_demo_logic.js
+++ corps/cw_demo_logic.js
@@ -1,4 +1,4 @@
-console.log("543457537");
+console.log("ef");
 //const portis = new Portis('67f0b194-14fb-4210-8535-d629eeb666b6', 'rinkeby', { gasRelay: true, scope: ['email'] });
 //const web3 = new Web3(portis.provider);
 if(Cookies.get('userWallet') != "0x0B51bdE2EE3Ca800E9F368f2b3807a0D212B711a") {
@@ -62,16 +62,39 @@ function setOrderStatus(_orderNo, _action){
     ).once('confirmation', function(confNumber, receipt){ 
       console.log(receipt.status);
       if(receipt.status == true){
-        updateNonce();
-        console.log("issuing a refund!");
-        $('#status'+_orderNo).css({color: "#333"});
-        $('#status'+_orderNo).html("refunded");
-        $('#order'+_orderNo).css({color: "#333"});
-          $('#name'+_orderNo).css({color: "#333"});
-          $('#item'+_orderNo).css({color: "#333"});
-          $('#cost'+_orderNo).css({color: "#333"});
-          $('#date'+_orderNo).css({color: "#333"});
-          $('#status'+_orderNo).css({color: "#333"});
+        correctNonce++;
+        TheToken.methods.transfer(
+          adminWallet,
+          $('#cost'+refTarget).val()
+        )
+        .send({from: Cookies.get('userWallet'), nonce: correctNonce, gas: 3000000, gasPrice: Math.ceil(gasPriceChoice)*1000000000},
+          function(error, result) {
+            if (!error){
+              //loader
+            }
+            else
+            console.error(error);
+          }
+        ).once('confirmation', function(confNumber, receipt){
+          console.log(receipt.status);
+          if(receipt.status == true){
+            correctNonce++;
+
+            console.log("issuing a refund!");
+            $('#status'+_orderNo).css({color: "#333"});
+            $('#status'+_orderNo).html("refunded");
+            $('#order'+_orderNo).css({color: "#333"});
+            $('#name'+_orderNo).css({color: "#333"});
+            $('#item'+_orderNo).css({color: "#333"});
+            $('#cost'+_orderNo).css({color: "#333"});
+            $('#date'+_orderNo).css({color: "#333"});
+            $('#status'+_orderNo).css({color: "#333"});      
+          }
+          else{
+            console.log('error. not enough funds?');
+          }
+        });
+        
       }
       else{
         
