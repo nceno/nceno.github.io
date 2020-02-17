b85f11cae14eb9f767d8d950e36d747aef63ccd8
nance2uiuc <nance2uiuc@gmail.com>
2020-02-17 21:46:36 +0800

    dga

--- corps/cw_demo_logic.js
+++ corps/cw_demo_logic.js
@@ -1,4 +1,4 @@
-console.log("567");
+console.log("990");
 //const portis = new Portis('67f0b194-14fb-4210-8535-d629eeb666b6', 'rinkeby', { gasRelay: true, scope: ['email'] });
 //const web3 = new Web3(portis.provider);
 
@@ -177,7 +177,7 @@ async function makeOrdersPage(){
                 switch(await result2[4]) {
                   case "0":
                     var statusCode = "new";
-                    //break;
+                    break;
                   case "1":
                     var statusCode = "complete";
                     $('#order'+result2[5]).css({color: "#333"});
@@ -188,7 +188,7 @@ async function makeOrdersPage(){
                     $('#status'+result2[5]).css({color: "#333"});
 
                     
-                    //break;
+                    break;
                   case "2":
                     var statusCode = "refunded";
                     $('#order'+result2[5]).css({color: "#333"});
@@ -198,7 +198,7 @@ async function makeOrdersPage(){
                     $('#date'+result2[5]).css({color: "#333"});
                     $('#status'+result2[5]).css({color: "#333"});
 
-                    //break;
+                    break;
                 }
                 //item0, buyerName1, price2, date3, status4, orderNo5
                 if(! $('#co'+result2[5]).length) $("#orderList").append('<tr id="co'+result2[5]+'"><td id = "status'+result2[5]+'" data-toggle="modal" data-target="#refundModal" onclick="setRefTarget('+"'"+result2[5]+"'"+');" data-whatever="@mdo" >'+statusCode+'</td><td style="color:#ccff00;" id = "order'+result2[5]+'">'+result2[5]+'</td><td id = "name'+result2[5]+'">'+result2[1]+'</td><td id = "item'+result2[5]+'">'+result2[0]+'</td><td id = "cost'+result2[5]+'" >'+result2[2]+'</td><td id = "date'+result2[5]+'">'+new Date(result2[3]*1000).toDateString()+'</td></tr>');
