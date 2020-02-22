04494056e1676851ffbc494747229cbfa281db96
nance2uiuc <nance2uiuc@gmail.com>
2020-02-23 00:21:23 +0800

    rgaea

--- corps/cw_demo_logic.js
+++ corps/cw_demo_logic.js
@@ -1,4 +1,4 @@
-console.log("2");
+console.log("4");
 //const portis = new Portis('67f0b194-14fb-4210-8535-d629eeb666b6', 'rinkeby', { gasRelay: true, scope: ['email'] });
 //const web3 = new Web3(portis.provider);
 
