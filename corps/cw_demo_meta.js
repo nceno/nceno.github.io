2e04076360c6598806d99ceb5203d7cca1370692
nance2uiuc <nance2uiuc@gmail.com>
2020-02-25 14:47:01 +0800

    hrw

--- corps/cw_demo_meta.js
+++ corps/cw_demo_meta.js
@@ -1,46 +1,46 @@
 const _goalID = '0xccff00';
 const companyID = '0xccff00'; 
-const companyName = "Suntek";
-const tokenAddress = '0xe3d06e15f286bcaaa28528b61da84737318eefc4'; 
-const TOKENSYMBOL = "SUN"; 
-const contractAddress = '0x57c9468b517d16aaab60753b981cdea317b192ef'; //change
-const adminWallet = '0x0B51bdE2EE3Ca800E9F368f2b3807a0D212B711a'; 
+const companyName = "Wingstop"; //
+const tokenAddress = '0xe3d06e15f286bcaaa28528b61da84737318eefc4'; //
+const TOKENSYMBOL = "WNG"; //
+const contractAddress = '0x57c9468b517d16aaab60753b981cdea317b192ef'; //
+const adminWallet = '0x0B51bdE2EE3Ca800E9F368f2b3807a0D212B711a'; //
 
-const adminUserID_1 = '49092681';
+const adminUserID_1 = '49092681'; //
 
 const inventory = 5;
 
 const item1={
-	name: 'Lunch delivery',
+	name: '6 wings',
 	//price : 620
-	price : 200,
-	descr : "Lunch delivery from restaurant of your choice"
+	price : 120,
+	descr : "6 wings of your choice"
 }
 
 const item2={
-	name: 'Parking space',
+	name: '2 pints',
 	//price: 700
-	price: 7,
-	descr : "Boss's parking space for 2 weeks"
+	price: 100,
+	descr : "2 Pints of draft beer"
 }
 
 const item3={
-	name: 'Half-day',
+	name: 'Dinner for 1',
 	//price: 2200
-	price: 2,
-	descr : "Half day of work"
+	price: 180,
+	descr : "10pc Dinner combo"
 }
 
 const item4={
-	name: 'Happy hour',
+	name: 'Beer mug',
 	//price: 450
-	price: 4,
-	descr : "Lunch time happy hour for your department"
+	price: 150,
+	descr : "Wingstop logo beer mug"
 }
 
 const item5={
-	name: 'Dinner for 2',
+	name: 'Bottle of sauce',
 	//price: 9500
 	price: 90,
-	descr : "Dinner for two at 101 signature room"
+	descr : "1 Bottle of gourmet wing sauce"
 }
