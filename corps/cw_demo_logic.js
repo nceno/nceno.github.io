dedc32e46ceee855bc58921323783c32062fea6d
nance2uiuc <nance2uiuc@gmail.com>
2020-02-28 18:51:32 +0800

    geg

--- corps/cw_demo_logic.js
+++ corps/cw_demo_logic.js
@@ -421,25 +421,25 @@ function makeWorkoutPage(){
                         //post to top if it's me
                           //if(! $("#me").length){
                             switch(pic[4]){
-                              case 'large.jpg':
+                              case "large.jpg":
                                 // same
                                 $("#me").prepend(
                                     '<h4 class="progress-title">'  +playerName+ '<font style="color:#ccff00;"> +' +theirReward+' '+TOKENSYMBOL+ '</font> / <font style="color:#f442b3;">' +theirKms+ 'km, '+theirMins+'mins</font></h4><div class="progress-item"><div class="progress"><div class="progress-bar bg-blue" role="progressbar" style="width:' +theirProgress+ '%;" aria-valuenow="' +theirProgress+ '" aria-valuemin="0" aria-valuemax="100"><span><figure class="user user-circle"><a href="https://www.strava.com/athletes/'+playerID+'" target="_blank"><img height="40" width="40" src="https://'+pic[0]+'.cloudfront.net/pictures/athletes/'+pic[1]+'/'+pic[2]+'/'+pic[3]+'/'+pic[4]+'"></a></figure> </span></div></div>'
                                   );
 
-                              case 'medium.jpg':
+                              case "medium.jpg":
                                 // same
                                 $("#me").prepend(
                                     '<h4 class="progress-title">'  +playerName+ '<font style="color:#ccff00;"> +' +theirReward+' '+TOKENSYMBOL+ '</font> / <font style="color:#f442b3;">' +theirKms+ 'km, '+theirMins+'mins</font></h4><div class="progress-item"><div class="progress"><div class="progress-bar bg-blue" role="progressbar" style="width:' +theirProgress+ '%;" aria-valuenow="' +theirProgress+ '" aria-valuemin="0" aria-valuemax="100"><span><figure class="user user-circle"><a href="https://www.strava.com/athletes/'+playerID+'" target="_blank"><img height="40" width="40" src="https://'+pic[0]+'.cloudfront.net/pictures/athletes/'+pic[1]+'/'+pic[2]+'/'+pic[3]+'/'+pic[4]+'"></a></figure> </span></div></div>'
                                   );
 
-                              case '0':
+                              case "0":
                                 // facebook
                                 $("#me").prepend(
                                     '<h4 class="progress-title">'  +playerName+ '<font style="color:#ccff00;"> +' +theirReward+' '+TOKENSYMBOL+ '</font> / <font style="color:#f442b3;">' +theirKms+ 'km, '+theirMins+'mins</font></h4><div class="progress-item"><div class="progress"><div class="progress-bar bg-blue" role="progressbar" style="width:' +theirProgress+ '%;" aria-valuenow="' +theirProgress+ '" aria-valuemin="0" aria-valuemax="100"><span><figure class="user user-circle"><a href="https://www.strava.com/athletes/'+playerID+'" target="_blank"><img height="40" width="40" src="https://graph.facebook.com/'+pic[0]+'/picture?height=256&width=256"></a></figure> </span></div></div>'
                                   );
 
-                              case 'default':
+                              case "default":
                                 // local icon
                                 $("#me").prepend(
                                     '<h4 class="progress-title">'  +playerName+ '<font style="color:#ccff00;"> +' +theirReward+' '+TOKENSYMBOL+ '</font> / <font style="color:#f442b3;">' +theirKms+ 'km, '+theirMins+'mins</font></h4><div class="progress-item"><div class="progress"><div class="progress-bar bg-blue" role="progressbar" style="width:' +theirProgress+ '%;" aria-valuenow="' +theirProgress+ '" aria-valuemin="0" aria-valuemax="100"><span><img height="40" width="40" src="../app/assets/images/avatar0.png"> </span></div></div>'
@@ -475,25 +475,25 @@ function makeWorkoutPage(){
                         //.after following entries
 
                         switch(pic[4]){
-                          case 'large.jpg':
+                          case "large.jpg":
                             // same
                             $('#startList').after(
                               '<div id="'+playerName.replace(/ /g,"_")+'" class="col-12 mt-2"><h4 class="progress-title">'  +playerName+ '<font style="color:#ccff00;"> +' +theirReward+' '+TOKENSYMBOL+ '</font> / <font style="color:#f442b3;">' +theirKms+ 'km, '+theirMins+'mins</font></h4><div class="progress-item"><div class="progress"><div class="progress-bar bg-blue" role="progressbar" style="width:' +theirProgress+ '%;" aria-valuenow="' +theirProgress+ '" aria-valuemin="0" aria-valuemax="100"><span><figure class="user user-circle"><a href="https://www.strava.com/athletes/'+playerID+'" target="_blank"><img height="40" width="40" src="https://'+pic[0]+'.cloudfront.net/pictures/athletes/'+pic[1]+'/'+pic[2]+'/'+pic[3]+'/'+pic[4]+'"></a></figure> </span></div></div></div></div>'
                             );
 
-                          case 'medium.jpg':
+                          case "medium.jpg":
                             // same
                             $('#startList').after(
                               '<div id="'+playerName.replace(/ /g,"_")+'" class="col-12 mt-2"><h4 class="progress-title">'  +playerName+ '<font style="color:#ccff00;"> +' +theirReward+' '+TOKENSYMBOL+ '</font> / <font style="color:#f442b3;">' +theirKms+ 'km, '+theirMins+'mins</font></h4><div class="progress-item"><div class="progress"><div class="progress-bar bg-blue" role="progressbar" style="width:' +theirProgress+ '%;" aria-valuenow="' +theirProgress+ '" aria-valuemin="0" aria-valuemax="100"><span><figure class="user user-circle"><a href="https://www.strava.com/athletes/'+playerID+'" target="_blank"><img height="40" width="40" src="https://'+pic[0]+'.cloudfront.net/pictures/athletes/'+pic[1]+'/'+pic[2]+'/'+pic[3]+'/'+pic[4]+'"></a></figure> </span></div></div></div></div>'
                             );
 
-                          case '0':
+                          case "0":
                             // facebook
                             $('#startList').after(
                               '<div id="'+playerName.replace(/ /g,"_")+'" class="col-12 mt-2"><h4 class="progress-title">'  +playerName+ '<font style="color:#ccff00;"> +' +theirReward+' '+TOKENSYMBOL+ '</font> / <font style="color:#f442b3;">' +theirKms+ 'km, '+theirMins+'mins</font></h4><div class="progress-item"><div class="progress"><div class="progress-bar bg-blue" role="progressbar" style="width:' +theirProgress+ '%;" aria-valuenow="' +theirProgress+ '" aria-valuemin="0" aria-valuemax="100"><span><figure class="user user-circle"><a href="https://www.strava.com/athletes/'+playerID+'" target="_blank"><img height="40" width="40" src="https://graph.facebook.com/'+pic[0]+'/picture?height=256&width=256"></a></figure> </span></div></div></div></div>'
                             );
 
-                          case 'default':
+                          case "default":
                             // local icon
                             $('#startList').after(
                               '<div id="'+playerName.replace(/ /g,"_")+'" class="col-12 mt-2"><h4 class="progress-title">'  +playerName+ '<font style="color:#ccff00;"> +' +theirReward+' '+TOKENSYMBOL+ '</font> / <font style="color:#f442b3;">' +theirKms+ 'km, '+theirMins+'mins</font></h4><div class="progress-item"><div class="progress"><div class="progress-bar bg-blue" role="progressbar" style="width:' +theirProgress+ '%;" aria-valuenow="' +theirProgress+ '" aria-valuemin="0" aria-valuemax="100"><span><img height="40" width="40" src="../app/assets/images/avatar0.png"> </span></div></div></div></div>'
