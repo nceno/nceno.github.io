5ad3dd923c568d64f5f6004fced031f153293f76
nance2uiuc <nance2uiuc@gmail.com>
2020-03-04 15:28:11 +0800

    rgaer

--- corps/cw_demo_logic.js
+++ corps/cw_demo_logic.js
@@ -689,7 +689,7 @@ async function makeWorkoutPage(){
                             $('#startList').after(
                               '<div id="'+playerName.replace(/ /g,"_")+'" class="col-12 mt-2"><h4 class="progress-title">'  +playerName+ '<font style="color:#ccff00;"> +' +theirReward+' '+TOKENSYMBOL+ '</font> / <font style="color:#f442b3;">' +theirKms+ 'km, '+theirMins+'mins</font></h4><div class="progress-item"><div class="progress"><div class="progress-bar bg-blue" role="progressbar" style="width:' +theirProgress+ '%;" aria-valuenow="' +theirProgress+ '" aria-valuemin="0" aria-valuemax="100"><span><figure class="user user-circle"><a href="https://www.strava.com/athletes/'+playerID+'" target="_blank"><img height="40" width="40" src="https://'+pic[0]+'.cloudfront.net/pictures/athletes/'+pic[1]+'/'+pic[2]+'/'+pic[3]+'/'+pic[4]+'"></a></figure> </span></div></div></div></div>'
                             );
-                            console.log("large " + playerName);
+                            console.log("medium " + playerName);
                             break;
 
                           case "0":
@@ -697,7 +697,7 @@ async function makeWorkoutPage(){
                             $('#startList').after(
                               '<div id="'+playerName.replace(/ /g,"_")+'" class="col-12 mt-2"><h4 class="progress-title">'  +playerName+ '<font style="color:#ccff00;"> +' +theirReward+' '+TOKENSYMBOL+ '</font> / <font style="color:#f442b3;">' +theirKms+ 'km, '+theirMins+'mins</font></h4><div class="progress-item"><div class="progress"><div class="progress-bar bg-blue" role="progressbar" style="width:' +theirProgress+ '%;" aria-valuenow="' +theirProgress+ '" aria-valuemin="0" aria-valuemax="100"><span><figure class="user user-circle"><a href="https://www.strava.com/athletes/'+playerID+'" target="_blank"><img height="40" width="40" src="https://graph.facebook.com/'+pic[0]+'/picture?height=256&width=256"></a></figure> </span></div></div></div></div>'
                             );
-                            console.log("large " + playerName);
+                            console.log("0 " + playerName);
                             break;
 
                           case "default":
@@ -705,7 +705,7 @@ async function makeWorkoutPage(){
                             $('#startList').after(
                               '<div id="'+playerName.replace(/ /g,"_")+'" class="col-12 mt-2"><h4 class="progress-title">'  +playerName+ '<font style="color:#ccff00;"> +' +theirReward+' '+TOKENSYMBOL+ '</font> / <font style="color:#f442b3;">' +theirKms+ 'km, '+theirMins+'mins</font></h4><div class="progress-item"><div class="progress"><div class="progress-bar bg-blue" role="progressbar" style="width:' +theirProgress+ '%;" aria-valuenow="' +theirProgress+ '" aria-valuemin="0" aria-valuemax="100"><span><img height="40" width="40" src="../app/assets/images/avatar0.png"> </span></div></div></div></div>'
                             );
-                            console.log("large " + playerName);
+                            console.log("default " + playerName);
                             break;
                         }
 
